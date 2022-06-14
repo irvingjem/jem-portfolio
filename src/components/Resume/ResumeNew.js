a6fa742f3eceb9afada898d32b950debf9ec45b5
Your Name <irving.jem@gmail.com>
2022-06-14 16:55:09 -0700

    trying to get resume to work

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/../Assets/Jem-Irving-Resume-CV";
+import pdf from "../../Assets/Jem_Irving_Resume_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/irvingjem/portfolio/master/src/Assets/.pdf";
+  "https://raw.githubusercontent.com/irvingjem/jem-protfolio/src/Assets/Jem_Irving_Resume_CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
