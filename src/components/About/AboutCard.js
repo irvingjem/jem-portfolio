d720780ac317a64dbfdbfeddff7c3a697b8329f6
Your Name <irving.jem@gmail.com>
2022-08-03 08:24:09 -0700

    fixed about section

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi I am <span>Jem Irving </span>
+            Hi my name is <span>Jem Irving </span>
             from <span> Portland, OR</span>
             <br />As a Developer and Designer who loves  to find the meeting point of technology and design, I am earning an intensive Full Stack Coding Certificate at the University of Oregon. I am gaining experience in the foundations of web development while honing my skills in HTML5, CSS, Bootstrap, and JavaScript, jQuery, MERN, & MySQL. My design skills include Adobe creative suite, UX/UI and Typography. I have a BFA in Communication Design from Pacific Northwest College of Art. I am someone who always wants to learn and enjoys problem-solving to create new solutions for making people's lives better. Looking to bring my attention to detail and technical skills to a company to grow and become a valuable asset.
             <br />
@@ -17,7 +17,7 @@ function AboutCard() {
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Video Games
             </li>
             <li className="about-activity">
               <ImPointRight /> Traveling
@@ -27,6 +27,9 @@ function AboutCard() {
             </li>
             <li className="about-activity">
               <ImPointRight /> Designing Logos
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading 
             </li>
           </ul>
         </blockquote>
